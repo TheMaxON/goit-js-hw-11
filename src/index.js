1970acdeee09eaef9700a6fbab54f5264e741c89
TheMaxON <sldpsnow8800@gmail.com>
2023-03-20 21:58:04 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import ImageSearch from './js/fetchImages';
+import InfiniteAjaxScroll from '@webcreate/infinite-ajax-scroll';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   searchInput: document.querySelector('[name="searchQuery"]'),
   searchBtn: document.querySelector('[name="searchBtn"]'),
   galleryEl: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  loadMoreBtnPending: document.querySelector('.pending-btn'),
 };
 
 const imageSearch = new ImageSearch();
 refs.searchForm.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onFormSubmit(e) {
   e.preventDefault();
@@ -22,34 +26,42 @@ function onFormSubmit(e) {
   }
 
   imageSearch.searchQuery = searchQuery;
+  imageSearch.resetPage();
+  clearMarkups();
+  imageAPICall();
+}
 
-  imageSearch.fetchImages().then(response => {
-    const images = response.hits;
+async function imageAPICall() {
+  await imageSearch.fetchImages().then(response => {
+    const { hits, totalHits } = response.data;
+    console.log(totalHits);
 
-    if (images.length === 0) {
+    if (hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
+    } else {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     }
-    imageSearch.resetPage();
-    galleryMarkup(images);
-    console.log(response);
+
+    galleryMarkup(hits);
   });
 }
 
 function galleryMarkup(images) {
-  const imagesGrid = images.map(
-    ({
-      webformatURL,
-      //   largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `
+  const imagesGrid = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
         <div class="photo-card">
         <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
@@ -67,7 +79,23 @@ function galleryMarkup(images) {
             </p>
         </div>
         </div>`;
-    }
-  );
-  refs.galleryEl.insertAdjacentHTML('beforeend', imagesGrid.join(''));
+      }
+    )
+    .join('');
+
+  refs.galleryEl.insertAdjacentHTML('beforeend', imagesGrid);
+  refs.loadMoreBtn.classList.remove('disabled');
+  refs.loadMoreBtnPending.classList.add('disabled');
+}
+
+function onLoadMore() {
+  refs.loadMoreBtn.classList.add('disabled');
+  refs.loadMoreBtnPending.classList.remove('disabled');
+  imageSearch.incrementPage();
+  imageAPICall();
+}
+
+function clearMarkups() {
+  refs.galleryEl.innerHTML = '';
+  refs.loadMoreBtn.classList.add('disabled');
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,19 +1,34 @@
+// const axios = require('axios');
+import axios from 'axios';
+
 export default class ImageSearch {
   constructor() {
+    this.baseUrl = 'https://pixabay.com/api/';
     this.apiKey = '34527262-b94b65b29daaf98e2e152eee9';
     this.searchQuery = '';
     this.page = 1;
   }
 
-  fetchImages() {
-    const apiUrl = `https://pixabay.com/api/?key=${this.apiKey}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
+  async fetchImages() {
+    // const apiUrl = `https://pixabay.com/api/?key=${this.apiKey}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
 
-    return fetch(apiUrl).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
+    try {
+      const response = await axios.get(`${this.baseUrl}`, {
+        params: {
+          key: this.apiKey,
+          q: this.searchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 40,
+          page: this.page,
+        },
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   incrementPage() {
